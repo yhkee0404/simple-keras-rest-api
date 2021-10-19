bb4e4d41cd9fb145a36810e8f35c22caab104046
yhkee0404 <yhkee0404@gmail.com>
2021-10-20 07:53:29 +0900

    Get url argument

--- run_keras_server.py
+++ run_keras_server.py
@@ -1,70 +1,42 @@
 # USAGE
 # Start the server:
-# 	python run_keras_server.py
+# 	python -m ai.simple-keras-rest-api.run_keras_server
 # Submit a request via cURL:
-# 	curl -X POST -F image=@dog.jpg 'http://localhost:5000/predict'
-# Submita a request via Python:
-#	python simple_request.py
+# 	curl -X POST -F wav=@ai/test.wav http://localhost:5000/score
+# Submit a request via Python:
+#	python -m ai.simple-keras-rest-api.simple_request.py ai/test.wav http://localhost:5000/score
 
 # import the necessary packages
-from keras.applications import ResNet50
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
-from PIL import Image
-import numpy as np
+from ..Multimodal.Audio_Inference import score, AudioClassifier, BERTClassifier, MultimodalClassifier
 import flask
 import io
+import os
 
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
-model = None
 
-def load_model():
-	# load the pre-trained Keras model (here we are using a model
-	# pre-trained on ImageNet and provided by Keras, but you can
-	# substitute in your own networks just as easily)
-	global model
-	model = ResNet50(weights="imagenet")
-
-def prepare_image(image, target):
-	# if the image mode is not RGB, convert it
-	if image.mode != "RGB":
-		image = image.convert("RGB")
-
-	# resize the input image and preprocess it
-	image = image.resize(target)
-	image = img_to_array(image)
-	image = np.expand_dims(image, axis=0)
-	image = imagenet_utils.preprocess_input(image)
-
-	# return the processed image
-	return image
-
-@app.route("/predict", methods=["POST"])
+@app.route("/score", methods=["POST"])
 def predict():
 	# initialize the data dictionary that will be returned from the
 	# view
 	data = {"success": False}
 
-	# ensure an image was properly uploaded to our endpoint
+	# ensure a wav was properly uploaded to our endpoint
 	if flask.request.method == "POST":
-		if flask.request.files.get("image"):
-			# read the image in PIL format
-			image = flask.request.files["image"].read()
-			image = Image.open(io.BytesIO(image))
+		if flask.request.files.get("wav"):
+			# read the wav in librosa
+			wav = flask.request.files["wav"].read()
+			data_file = io.BytesIO(wav)
 
-			# preprocess the image and prepare it for classification
-			image = prepare_image(image, target=(224, 224))
-
-			# classify the input image and then initialize the list
+			# preprocess the wav and prepare it for classification
+			# classify the input wav and then initialize the list
 			# of predictions to return to the client
-			preds = model.predict(image)
-			results = imagenet_utils.decode_predictions(preds)
+			result = score(data_file)
 			data["predictions"] = []
 
 			# loop over the results and add them to the list of
 			# returned predictions
-			for (imagenetID, label, prob) in results[0]:
+			for (label, prob) in result:
 				r = {"label": label, "probability": float(prob)}
 				data["predictions"].append(r)
 
@@ -79,5 +51,6 @@ def predict():
 if __name__ == "__main__":
 	print(("* Loading Keras model and Flask starting server..."
 		"please wait until server has fully started"))
-	load_model()
+	os.chdir('ai/Multimodal')
+	score(open('../test.wav','rb'))
 	app.run()
--- simple_request.py
+++ simple_request.py
@@ -3,15 +3,16 @@
 
 # import the necessary packages
 import requests
+import sys
 
 # initialize the Keras REST API endpoint URL along with the input
-# image path
-KERAS_REST_API_URL = "http://localhost:5000/predict"
-IMAGE_PATH = "dog.jpg"
+# wav path
+KERAS_REST_API_URL = sys.argv[2]
+WAV_PATH = sys.argv[1]
 
-# load the input image and construct the payload for the request
-image = open(IMAGE_PATH, "rb").read()
-payload = {"image": image}
+# load the input wav and construct the payload for the request
+wav = open(WAV_PATH, "rb").read()
+payload = {"wav": wav}
 
 # submit the request
 r = requests.post(KERAS_REST_API_URL, files=payload).json()
@@ -22,6 +23,7 @@ if r["success"]:
 	for (i, result) in enumerate(r["predictions"]):
 		print("{}. {}: {:.4f}".format(i + 1, result["label"],
 			result["probability"]))
+	print(r)
 
 # otherwise, the request failed
 else:
